045ad468f4c8be2811f6bbf8674867b67015d46d
Jonathon Doran <jonathon.doran@unt.edu>
2026-04-19 19:15:07 -0500

    Added packet handlers for HP and Mana updates.

---
+++ Glass/Network/Handlers/HandleHpUpdate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Buffers.Binary;
+using Glass.Core;
+using Glass.Network.Protocol;
+
+namespace Glass.Network.Handlers;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// HandleHpUpdate
+//
+// Handles OP_PlayerProfile packets.  
+///////////////////////////////////////////////////////////////////////////////////////////////
+public class HandleHpUpdate : IHandleOpcodes
+{
+    private ushort _opcode = 0x8f45;
+    private readonly string _opcodeName = "OP_HpUpdate";
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Opcode
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public ushort Opcode
+    {
+        get { return _opcode; }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // OpcodeName
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public string OpcodeName
+    {
+        get { return _opcodeName; }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // HandlePacket
+    //
+    // Dispatches to direction-specific handlers.
+    //
+    // data:       The application payload
+    // length:     Length of the application payload
+    // direction:  Direction byte
+    // opcode:     The application-level opcode
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public void HandlePacket(ReadOnlySpan<byte> data, int length,
+                              byte direction, ushort opcode, PacketMetadata metadata)
+    {
+        if (direction == SoeConstants.DirectionServerToClient)
+        {
+            HandleServerToClient(data, length, metadata);
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // HandleServerToClient
+    //
+    // Processes zone-to-client traffic
+    //
+    // data:    The application payload
+    // length:  Length of the application payload
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private void HandleServerToClient(ReadOnlySpan<byte> data, int length, PacketMetadata metadata)
+    {
+        if (length < 18)
+        {
+            DebugLog.Write("HPUpdate packet less than 18 bytes.  This is unusual");
+            return;
+        }
+
+        int playerId = BinaryPrimitives.ReadInt16BigEndian(data.Slice(0));
+        int currentHP = BinaryPrimitives.ReadInt32LittleEndian(data.Slice(2));
+        int maxHP = BinaryPrimitives.ReadInt32LittleEndian(data.Slice(10));
+
+
+
+        DebugLog.Write("[" + metadata.Timestamp.ToString("HH:mm:ss.fff") + "] "
+            + _opcodeName + " length=" + length);
+        DebugLog.Write("HP at " + currentHP + " / " + maxHP);
+    }
+}
+
+
---
+++ Glass/Network/Handlers/HandleManaUpdate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Buffers.Binary;
+using Glass.Core;
+using Glass.Network.Protocol;
+
+namespace Glass.Network.Handlers;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// HandleManaUpdate
+//
+// Handles OP_PlayerProfile packets.  
+///////////////////////////////////////////////////////////////////////////////////////////////
+public class HandleManaUpdate : IHandleOpcodes
+{
+    private ushort _opcode = 0x37ad;
+    private readonly string _opcodeName = "OP_ManaUpdate";
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Opcode
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public ushort Opcode
+    {
+        get { return _opcode; }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // OpcodeName
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public string OpcodeName
+    {
+        get { return _opcodeName; }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // HandlePacket
+    //
+    // Dispatches to direction-specific handlers.
+    //
+    // data:       The application payload
+    // length:     Length of the application payload
+    // direction:  Direction byte
+    // opcode:     The application-level opcode
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    public void HandlePacket(ReadOnlySpan<byte> data, int length,
+                              byte direction, ushort opcode, PacketMetadata metadata)
+    {
+        if (direction == SoeConstants.DirectionServerToClient)
+        {
+            HandleServerToClient(data, length, metadata);
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // HandleServerToClient
+    //
+    // Processes zone-to-client traffic
+    //
+    // data:    The application payload
+    // length:  Length of the application payload
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private void HandleServerToClient(ReadOnlySpan<byte> data, int length, PacketMetadata metadata)
+    {
+        if (length < 10)
+        {
+            DebugLog.Write("ManaUpdate packet less than 10 bytes.  This is unusual");
+            return;
+        }
+
+        int playerId = BinaryPrimitives.ReadInt16BigEndian(data.Slice(0));
+        int currentMana = BinaryPrimitives.ReadInt32LittleEndian(data.Slice(2));
+        int maxMana = BinaryPrimitives.ReadInt32LittleEndian(data.Slice(6));
+
+
+
+        DebugLog.Write("[" + metadata.Timestamp.ToString("HH:mm:ss.fff") + "] "
+            + _opcodeName + " length=" + length);
+        DebugLog.Write("Mana at " + currentMana + " / " + maxMana);
+    }
+
+    
+}
+
+
